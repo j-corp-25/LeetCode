ce083bc894b68be82b298114963caead7a6f647b
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2023-10-18 10:13:06 +0200

    Time: 65 ms (77.80%), Space: 43.3 MB (74.45%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+// Big O( s + t)
+// Memory (s + t)
+var isAnagram = function(s, t) {
+    if (s.length !== t.length){
+        return false
+    }
+    
+    let countS = {}
+    let countT = {}
+    
+    for(i = 0; i < s.length; i++){
+        if(!countS[s[i]]){
+            countS[s[i]] = 1
+        }else {
+            countS[s[i]] += 1 
+        }
+    }
+    for(j = 0; j < t.length; j++){
+        if(!countT[t[j]]) {
+            countT[t[j]] = 1
+        }else {
+            countT[t[j]] += 1
+        }
+    }
+    for (let key in countS) {
+        if (countS[key] !== countT[key]) {
+            return false;
+        }
+    }
+
+    return true;
+    
+};
