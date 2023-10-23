8c697ed5331fe3ba94337204aae63b5d78782fb7
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2023-10-22 20:33:55 -0400

    Time: 85 ms (39.16%), Space: 54.9 MB (40.31%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -5,15 +5,27 @@
  */
 var findMaxAverage = function(arr, k) {
     
+    // initialize the sum of the current window [  ]
     let windowSum = 0;
+    
+    // initialize a pointer to the start of the window\
     let windowStart = 0;
+    
+    //this is just to the get the smallest possible values. it is a good tactic incase values are less than 0
     let maxAverage = -Infinity; 
     
+    // loop through each element in the array with an index "windowEnd"
     for(let windowEnd = 0; windowEnd < arr.length; windowEnd++){
+        
+        //now we add the current number to the windows sum
         windowSum += arr[windowEnd];
         
+        //make our window up to the size K and less than one to not get any errors
         if(windowEnd >= k - 1){
+            
+            // calculate the average of current window and update maxAverage if needed
             maxAverage = Math.max(maxAverage, windowSum/k);
+            
             windowSum -= arr[windowStart];
             windowStart++;
         }
