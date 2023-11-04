b6a0d827df4d7358ca1f800d256a5d4c10679e9a
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2023-11-04 15:18:04 -0400

    Time: 236 ms (19.6%), Space: 29.4 MB (61.55%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        minLength = float('inf')
+        windowStart = 0
+        windowSum = 0
+        
+        for windowEnd in range(len(nums)):
+            windowSum += nums[windowEnd]
+            
+#             go into the while loop if windowSum is greater than teh target
+            while windowSum >= target:
+#                 make the window inclusive by inclusing all the elements
+                minLength = min(minLength, windowEnd - windowStart + 1)
+                windowSum -= nums[windowStart]
+                windowStart += 1
+            
+#             if the minLength is still infinity then return 0
+        if minLength == inf:
+            return 0
+        
+        return minLength
+                
