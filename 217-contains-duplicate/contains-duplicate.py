355051db220ab3b260d63cd640260a4e3bf69ac0
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2023-11-18 12:11:06 -0500

    Time: 493 ms (22.69%) | Memory: 28.4 MB (93.15%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -8,24 +8,24 @@ class Solution:
 # This is the brute force of solving this problem, we compare each element in the array with the next element
 # The time complex is O(n^2) because there is a nested loop
 # The space complex is constant time because we only need to store a few variables
-        unique_set = set()
+        # unique_set = set()
 
-        for num in nums:
-            if num in unique_set:
-                return True
-            unique_set.add(num)
-        return False
+        # for num in nums:
+        #     if num in unique_set:
+        #         return True
+        #     unique_set.add(num)
+        # return False
 # # This uses a set to check if there are any duplicates in the array
 # We add a the new number to the set if its not in it in the first place but if it is we can return True
 # Time complex is O(n) because we only iterate through the entire thing once 
 # The space is O(n) because we are storing the values in the set
 
-        # nums.sort()
+        nums.sort()
 
-        # for i in range(len(nums) - 1):
-        #     if nums[i] == nums[i + 1]:
-        #         return True
-        # return False
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return True
+        return False
 
 # This sorts the algo beforehand which is n log n
 # when a list is sorted it duplicates appear next to each other so we check if the next element is the same
