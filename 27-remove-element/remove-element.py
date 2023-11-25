d656add7fc7b45ee734c77e72f174d39524387ae
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2023-11-24 21:26:47 -0500

    Time: 45 ms (24.14%) | Memory: 16.2 MB (42.91%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,10 +1,15 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+
         k = 0
 
         for i in range(len(nums)):
             if nums[i] != val:
+            # if the current value at the index does not equal the val then proceed
                 nums[k] = nums[i]
+                # use the value of current k for nums array
                 k += 1
         return k
+
+
         
