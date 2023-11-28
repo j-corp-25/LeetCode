41c3df3a880b31a13f32b713f1b66b8859c88486
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2023-11-27 21:13:31 -0500

    Time: 35 ms (94.73%) | Memory: 16.4 MB (29.37%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,21 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        #get the last the index of nums1
+        last = n + m - 1
 
-        last = m + n - 1
-
-        # merge in reverse order
-
-        while m > 0 and n > 0:
+        while n > 0 and m > 0:
             if nums1[m - 1] > nums2[n - 1]:
                 nums1[last] = nums1[m - 1]
                 m -= 1
             else:
-                nums1[last] = nums2[n - 1]
+                nums1[last] = nums2[n-1]
                 n -= 1
             last -= 1
         while n > 0:
             nums1[last] = nums2[n - 1]
             n -= 1
             last -= 1
+
+           
+
+
+        
