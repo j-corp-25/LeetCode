8a41fac0741af455ed81f4083a7eea54291183a3
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2023-12-07 00:50:46 -0500

    Time: 71 ms (14.49%) | Memory: 17.8 MB (79.82%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.py
@@ -2,8 +2,11 @@ class Solution:
     def largestOddNumber(self, num: str) -> str:
         i = len(num) - 1
 
+        # iterate from right, then keep moving left as each number is not odd
         while i >= 0:
+            # if this equals 1 it will be truthy else, if its 0 it will falsey then dexrease
             if int(num[i]) % 2:
+              # start will b 0, and it will end at the latest number, since its exclusive of last number you need to add one
                 return num[:i + 1]
             i -= 1
         return ""
