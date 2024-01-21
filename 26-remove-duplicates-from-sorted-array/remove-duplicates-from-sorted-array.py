03cfc1635a946d321c4719bfa26016386fc026d5
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2024-01-20 19:44:02 -0500

    Time: 76 ms (79.37%) | Memory: 17.8 MB (94.69%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,11 +1,16 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        # we need two pointers
-
+        # loop through array
+        # check if the first number and the one next to it is equal
+        # then switch them
+        left = 0
         right = 1
 
-        for i in range(1,len(nums)):
-            if nums[i] != nums[right - 1]:
-                nums[right] = nums[i]
-                right += 1
-        return right
+        for i in range(len(nums) - 1):
+            if nums[left] != nums[right]:
+                left += 1
+                nums[left] = nums[right]
+            right += 1
+        return left + 1
+        
+                
