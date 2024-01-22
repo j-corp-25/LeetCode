003d461e0419597c629373e2aad208ec13696984
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2024-01-21 23:59:25 -0500

    Time: 61 ms (99.60%) | Memory: 18.1 MB (68.28%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -3,14 +3,12 @@ class Solution:
         # loop through array
         # check if the first number and the one next to it is equal
         # then switch them
-        left = 0
-        right = 1
-
-        for i in range(len(nums) - 1):
-            if nums[left] != nums[right]:
+        left = 1
+        
+        for r in range(1,len(nums)):
+            if nums[r] != nums[r - 1]:
+                nums[left] = nums[r]
                 left += 1
-                nums[left] = nums[right]
-            right += 1
-        return left + 1
+        return left
         
                 
