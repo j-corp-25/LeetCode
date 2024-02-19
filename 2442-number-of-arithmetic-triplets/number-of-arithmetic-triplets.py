f91638fbe6a934552d2d507ba3aec9954f05407c
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2024-02-18 23:54:23 -0500

    Time: 37 ms (90.09%) | Memory: 16.4 MB (99.80%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.py
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.py
@@ -1,8 +1,13 @@
 class Solution:
     def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
         s = set(nums)
+        # put all the numbers in a set {1,3,5,7,9}
         count = 0
+        # return a count at the end
         for num in nums:
+            # iterate through each number in the nums array
             if (num + diff) in s and (num + 2*diff) in s:
+                # here we check if 1 + 2 = 3 is in the set which it is. then we check 1 + 2*2 = 5. 
+                # 
                 count += 1
         return count
