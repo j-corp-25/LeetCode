3e6e2f13e4a6a9deac0987f5d134f214f55faedb
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2024-02-19 09:07:26 -0500

    Time: 42 ms (75.64%) | Memory: 16.7 MB (60.73%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.py
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.py
@@ -4,9 +4,9 @@ class Solution:
         # put all the numbers in a set {1,3,5,7,9}
         count = 0
         # return a count at the end
-        for num in nums:
+        for i in range(len(nums)):
             # iterate through each number in the nums array
-            if (num + diff) in s and (num + 2*diff) in s:
+            if (nums[i] + diff) in s and (nums[i] + 2*diff) in s:
                 # here we check if (current num)1 + 2 = 3 is in the set which it is. then we check 1 + 2*2 = 5. which is also in the set we then increment count
                 count += 1
         return count
