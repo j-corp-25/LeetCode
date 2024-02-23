cccdfa86cb5362e2020bce00e762817c5ae54f0d
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2024-02-23 11:52:57 -0500

    Time: 54 ms (38.08%) | Memory: 17 MB (63.90%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -4,15 +4,20 @@ class Solution:
         # return true if every character is used atleast once, otherwise return false
         # I can use two hashes and then compare their key value pairs if they are equal
 
+
+        # set up two hashes
         countS, countT = {},{}
 
+        # check the length because anagrams always have the same length
         if len(s) != len(t):
             return False
         
+        # create a for loop to iterate through one of the characters then add it to the hash
         for i in range(len(s)):
             countS[s[i]] = countS.get(s[i],0) + 1
             countT[t[i]] = countT.get(t[i],0) + 1
 
+        # create another loop to check if the keys match dont match. if they dont then return false
         for key in countS:
             if countS[key] != countT.get(key,0):
                 return False
