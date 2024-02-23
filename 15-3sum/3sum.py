45b4ccc60213497830997b264a80ababe3b10b34
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2024-02-23 14:42:26 -0500

    Time: 663 ms (69.17%) | Memory: 20.9 MB (53.33%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,29 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
         nums.sort()
-        # [-4,-1,-1,0,1,2]
-
-        # we need "three" pointers, a left and right to check the solution
-        for i, a in enumerate(nums):
+        
+        newArray = []
+        for i, a in  enumerate(nums):
             if i > 0 and a == nums[i - 1]:
                 continue
-                
-            l,r = i + 1, len(nums) - 1
-            while l < r:
-                three_sum = a + nums[l] + nums[r]
-                if three_sum > 0:
-                    r -= 1
-                elif three_sum < 0:
-                    l += 1
+            left_pointer, right_pointer = i + 1, len(nums) - 1
+            while left_pointer < right_pointer:
+                threeSum = a + nums[left_pointer] + nums[right_pointer]
+                if threeSum > 0:
+                    right_pointer -= 1
+                elif threeSum < 0:
+                    left_pointer += 1
                 else:
-                    res.append([a,nums[l],nums[r]])
-                    l += 1
-                    while l < r and nums[l] == nums[l - 1]:
-                        l += 1
-        return res
+                    newArray.append([a,nums[left_pointer],nums[right_pointer]])
+                    left_pointer += 1
+                    while nums[left_pointer] == nums[left_pointer - 1] and left_pointer < right_pointer:
+                        left_pointer += 1
+        return newArray
+
+
+
 
 
 
         
-
