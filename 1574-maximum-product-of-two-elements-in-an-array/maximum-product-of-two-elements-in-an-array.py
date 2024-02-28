3fa72fdfdf985da935e2f6cd6cd1ee2b4a465e96
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2024-02-28 14:20:11 -0500

    Time: 54 ms (45.44%) | Memory: 16.6 MB (92.83%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.py
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.py
@@ -1,6 +1,8 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
+        # sort the algorithm
         nums.sort()
+        # get the last element and the element before the last with two pointers
         r = len(nums) - 1
         l = r - 1
         return ((nums[r] - 1) * (nums[l] - 1))
