48b4bc12b7b935db9628978cb99f0ddde219abbd
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2024-02-28 13:59:33 -0500

    Time: 63 ms (56.04%) | Memory: 17.9 MB (46.76%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,15 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # input array, input target - int
-        # find the index of two values that add to target
-        # method:
-        # create a empty hash to
         myMap = {}
-        # create a loop that will iterate through the numbers and then check if the difference is inside the  hash
+
         for i in range(len(nums)):
             diff = target - nums[i]
             if diff in myMap:
                 return [myMap[diff],i]
             myMap[nums[i]] = i
 
-        # if it is then return the indexes
+
+
+        
+        
