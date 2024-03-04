7810e7da0fd4ab8fb267b291a872850bab1d4c9e
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2024-03-03 22:54:57 -0500

    Time: 46 ms (91.06%) | Memory: 16.7 MB (64.63%) - LeetSync

---
+++ 985-bag-of-tokens/bag-of-tokens.py
@@ -0,0 +1,19 @@
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        tokens.sort()
+        l,r = 0, len(tokens) - 1
+        current_score = max_score = 0
+        while l <= r:
+            if tokens[l] <= power:
+                power -= tokens[l]
+                current_score += 1
+                max_score = max(max_score, current_score)
+                l += 1
+            elif current_score >= 1:
+                power += tokens[r]
+                current_score -= 1
+                r -= 1
+            else:
+                break
+        return max_score
+        
