8dd7e063f0a5878c4020950ec48dea144ec7daa4
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2024-03-05 00:21:09 -0500

    Time: 96 ms (31.68%) | Memory: 22.5 MB (8.68%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,13 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        strs_table = {}
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            res[tuple(count)].append(s)
+        return res.values()
 
-        for string in strs:
-            sorted_string = ''.join(sorted(string))
-
-            if sorted_string not in strs_table:
-                strs_table[sorted_string] = []
-
-            strs_table[sorted_string].append(string)
-
-        return list(strs_table.values())
+        
