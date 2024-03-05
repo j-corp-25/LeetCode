60421079fb14e95f3911d55b4b98dc565d1a572a
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2024-03-05 08:28:20 -0500

    Time: 84 ms (79.82%) | Memory: 19.7 MB (80.53%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,11 +1,9 @@
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
-        for s in strs:
-            count = [0] * 26
-            for c in s:
-                count[ord(c) - ord("a")] += 1
-            res[tuple(count)].append(s)
-        return res.values()
-
+    def groupAnagrams(self, strs):
+        anagram_map = defaultdict(list)
         
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+        
+        return list(anagram_map.values())
