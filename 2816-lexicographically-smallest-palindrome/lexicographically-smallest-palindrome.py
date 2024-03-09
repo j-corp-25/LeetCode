8c7cc9248fa3ebabb39319f5ab20955ae06822bb
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2024-03-09 18:20:44 -0500

    Time: 119 ms (46.60%) | Memory: 16.8 MB (48.63%) - LeetSync

---
+++ 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.py
@@ -0,0 +1,23 @@
+class Solution:
+    def makeSmallestPalindrome(self, s: str) -> str:
+        l = 0
+        r = len(s) - 1
+        Str = list(s)
+
+        while l < r:
+            left_let = ord(Str[l])
+            right_let = ord(Str[r])
+            if Str[l] == Str[r]:
+                l += 1
+                r -= 1
+            elif Str[l] != Str[r] and left_let < right_let:
+                Str[r] = Str[l]
+                l += 1
+                r -= 1
+            else:
+                Str[l] = Str[r]
+                l += 1
+                r -= 1
+            
+        return "".join(Str)
+        
