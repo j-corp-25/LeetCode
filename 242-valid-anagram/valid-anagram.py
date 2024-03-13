52a87fcb7e320e92f9a5632fc3badc5dd3b711ae
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2024-03-13 01:13:59 -0400

    Time: 58 ms (16.67%) | Memory: 16.9 MB (92.62%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,27 +1,25 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # two inputs. s string and t string
-        # return true if every character is used atleast once, otherwise return false
-        # I can use two hashes and then compare their key value pairs if they are equal
+        myHashA, myHashB = {}, {}
 
-
-        # set up two hashes
-        countS, countT = {},{}
-
-        # check the length because anagrams always have the same length
         if len(s) != len(t):
             return False
-        
-        # create a for loop to iterate through one of the characters then add it to the hash
-        for i in range(len(s)):
-            countS[s[i]] = countS.get(s[i],0) + 1
-            countT[t[i]] = countT.get(t[i],0) + 1
 
-        # create another loop to check if the keys match dont match. if they dont then return false
-        for key in countS:
-            if countS[key] != countT.get(key,0):
-                return False
-        return True
+        for char in s:
+            if char in myHashA:
+                myHashA[char] += 1
+            else:
+                myHashA[char] = 1
 
-        
+        for char in t:
+            if char in myHashB:
+                myHashB[char] += 1
+            else:
+                myHashB[char] = 1
+                
+        if myHashB == myHashA:
+            return True
+
+
+
         
