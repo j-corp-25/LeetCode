e603cc152d96608e3eed8edb2f9eea37030b198a
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2024-03-19 19:36:36 -0400

    Time: 342 ms (97.87%) | Memory: 17.2 MB (23.40%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.py
@@ -0,0 +1,6 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        counts = list(Counter(tasks).values())
+        most_repeats = max(counts)
+        num_longest = counts.count(most_repeats)
+        return max(len(tasks), (most_repeats-1) * (n+1) + num_longest)
