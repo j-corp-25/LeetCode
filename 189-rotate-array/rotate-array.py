ab19e552331b7829bffc1115724ff7bf39a3f639
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2024-03-22 12:02:16 -0400

    Time: 1132 ms (10.09%) | Memory: 28.1 MB (36.84%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,24 +3,48 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # we can probably use a stack/queue approach?
+        # grab the last elements base on k, [5,6,7]
+        #[:-3]
+        # grab the last 3
+        # shift them into the front the array
+        # pop the the last 3
+        # if k == 0 return array
+        # nums == 6, k == 6.
+        # 8 % len(nums) == 2
+        # last 2 elements
 
-        k = k % len(nums)
-        l,r = 0, len(nums) - 1
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l, r = l + 1, r - 1
-
-        l,r = 0, k - 1
-        while l < r:
-            nums[l],nums[r] = nums[r],nums[l]
-            l,r = l + 1, r - 1
+        if k == len(nums):  # [1,2,3,4,5,6,7], k = 3 , len(nums) = 7
+            return nums
+        if k == 0:
+            return nums
+        if k > len(nums):
+            k = k % len(nums)
         
-        l, r = k, len(nums) - 1
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l, r = l + 1, r - 1
+        lastKs = nums[-k:] # [5,6,7], k = 3
+        for i in range(len(lastKs)): # [5,6,7], i = 1, lastKs[1] = 6
+            nums.insert(i,lastKs[i]) # 0, lastKs[0] == 5 , [5,1,2,3,4,5,6,7], [5,6,1,2,3,4,5,6]
+            nums.pop() # [5,1,2,3,4,5,6] , [5,6,1,2,3,4,5], [5,6,7,1,2,3,4]
         
 
+
+
+
+
+ # [1,2,3,4,5,6,7], k = 3 
+
+ # step 1 : reverse array
+ # [7,6,5,4,3,2,1]
+ #step 2 : reverse  first k elements
+ # nums[:k] == reversed(nums[:k])
+# [5,6,7]
+#then reverse from k + 1, remaining in nums
+# from k up to len(nums)
+# [5,6,7,1,2,3,4]
+
+ 
+
+        
+
+          
 
         
