e44dd6668c3b961959e887c9a9eeca894776b093
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2024-04-01 11:33:37 -0400

    Time: 74 ms (5.09%) | Memory: 16.9 MB (91.84%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -5,9 +5,31 @@ class Solution:
         # only alphanumeric
         # A-Z,a-z, 0-9, ignore spaces, and anything outside of specified conditions
 
-        # Easy solution
-        new_str = ""
-        for c in s:
-            if c.isalnum():
-                new_str += c.lower()
-        return new_str == new_str[::-1]
+        # Easy solution #1
+        # new_str = ""
+        # for c in s:
+        #     if c.isalnum():
+        #         new_str += c.lower()
+        # return new_str == new_str[::-1]
+
+        # Medium solution #1
+
+        left, right = 0,len(s) - 1
+        while left < right:
+            while left < right and self.checkifAlphanum(s[left]) == False:
+                left += 1
+            while right > left and self.checkifAlphanum(s[right]) == False:
+                right -= 1
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
+            
+
+    def checkifAlphanum(self, char):
+        return (
+            ord("A") <= ord(char) <= ord("Z")
+            or ord("0") <= ord(char) <= ord("9")
+            or ord("a") <= ord(char) <= ord("z")
+        )
