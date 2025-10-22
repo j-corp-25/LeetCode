76ebbf82a5c7e3b3d098e485b9dc7f33c22c9aea
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2025-10-22 00:27:33 -0400

    Create 2540-MinimumCommonValue

---
+++ 2540-MinimumCommonValue/2540-MinimumCommonValue.js
@@ -0,0 +1,21 @@
+// Last updated: 10/22/2025, 12:27:32 AM
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var getCommon = function(nums1, nums2) {
+    let left1 = 0
+    let left2 = 0
+
+    if(nums1[nums1.length-1] < nums2[0] || nums2[nums2.length-1] < nums1[0]){
+        return -1
+    }
+    while(left1 < nums1.length && left2 < nums2.length){
+        if(nums1[left1] === nums2[left2]) return nums1[left1]
+        if(nums1[left1] > nums2[left2]) left2++
+        else left1++
+    }
+    return -1
+
+};
