171f434ea94fcdeb3a8272f6a31058278f704b30
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2025-10-22 00:09:35 -0400

    Create 424-LongestRepeatingCharacterReplacement

---
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.py
@@ -0,0 +1,23 @@
+# Last updated: 10/22/2025, 12:09:34 AM
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        charFreq = {}
+        maxLength = 0
+        windowStart = 0
+        maxCount = 0
+
+        for windowEnd in range(len(s)):
+            endChar = s[windowEnd]
+            if endChar not in charFreq:
+                charFreq[endChar] = 0
+            charFreq[endChar] += 1
+
+            maxCount = max(maxCount, charFreq[endChar])
+
+            while (windowEnd - windowStart + 1) - maxCount > k:
+                startChar = s[windowStart]
+                charFreq[startChar] -= 1
+                windowStart += 1
+            maxLength = max(maxLength,windowEnd - windowStart + 1)
+        return maxLength
+        
