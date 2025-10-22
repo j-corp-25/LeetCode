fcdfd947475222f9f6fe5c6ffbc65497a80f8b9e
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2025-10-22 00:09:21 -0400

    Create 861-FlippingAnImage

---
+++ 861-FlippingAnImage/861-FlippingAnImage.py
@@ -0,0 +1,21 @@
+# Last updated: 10/22/2025, 12:09:21 AM
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        # x is the row y is the column
+        # loop through each regular array [...] and possibly reverse method
+        # loop through it again and then inverse the number based on what it is
+        # the number is 1 then it will 0 and vice versa
+        # then return the inverted image
+
+        for i in range(len(image)):
+            image[i] = [1 - x for x in image[i][::-1]]
+        return image
+           
+                
+
+
+
+
+
+        
+        
