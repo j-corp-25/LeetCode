34c59b330f769be473269e70d488aa7f1ea4bee7
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2025-10-22 00:09:29 -0400

    Create 567-PermutationInString

---
+++ 567-PermutationInString/567-PermutationInString.py
@@ -0,0 +1,31 @@
+# Last updated: 10/22/2025, 12:09:29 AM
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        charFreq = {}
+        isMatch = 0
+        windowStart = 0
+
+        for char in s1:
+            if char not in charFreq:
+                charFreq[char] = 0
+            charFreq[char] += 1
+
+        for windowEnd in range(len(s2)):
+            rightChar = s2[windowEnd]
+            if rightChar in charFreq:
+                charFreq[rightChar] -= 1
+                if charFreq[rightChar] == 0:
+                    isMatch += 1
+                
+            if windowEnd >= len(s1) - 1:
+                if isMatch == len(charFreq):
+                    return True
+
+                leftChar = s2[windowStart]
+                windowStart += 1
+                if leftChar in charFreq:
+                    if charFreq[leftChar] == 0:
+                        isMatch -= 1
+                    charFreq[leftChar] += 1
+        return False
+        
