ddaae63739d7fa2a3b42686cfe8195733dc92799
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2025-10-22 00:09:22 -0400

    Create 807-CustomSortString

---
+++ 807-CustomSortString/807-CustomSortString.py
@@ -0,0 +1,21 @@
+# Last updated: 10/22/2025, 12:09:22 AM
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        
+        char_count = {char: 0 for char in order}
+        for char in s:
+            if char in char_count:
+                char_count[char] += 1
+    
+        sorted_s = ''
+        for char in order:
+            sorted_s += char * char_count[char]
+    
+        for char in s:
+            if char not in order:
+                sorted_s += char
+
+        return sorted_s
+            
+        
+        
