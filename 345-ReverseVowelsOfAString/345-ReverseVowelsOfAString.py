6af1952714a4bb9223d2ef753dbabdac158fc794
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2025-10-22 00:09:37 -0400

    Create 345-ReverseVowelsOfAString

---
+++ 345-ReverseVowelsOfAString/345-ReverseVowelsOfAString.py
@@ -0,0 +1,24 @@
+# Last updated: 10/22/2025, 12:09:36 AM
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+
+        vowels = "aeiouAEIOU"
+
+        first,last = 0,len(s) - 1
+
+        array = list(s)
+
+        while first < last:
+            while first < last and vowels.find(array[first]) == -1:
+                first += 1
+            
+            while first < last and vowels.find(array[last]) == -1:
+                last -= 1
+
+            array[first],array[last] = array[last],array[first]
+            first += 1
+            last -= 1
+        return "".join(array)
+            
+            
+        
