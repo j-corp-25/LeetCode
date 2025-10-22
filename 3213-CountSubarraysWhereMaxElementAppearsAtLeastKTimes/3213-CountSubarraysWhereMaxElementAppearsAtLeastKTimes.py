8363d18c8e2c11e51608195b4a85b7732fab39be
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2025-10-22 00:08:45 -0400

    Create 3213-CountSubarraysWhereMaxElementAppearsAtLeastKTimes

---
+++ 3213-CountSubarraysWhereMaxElementAppearsAtLeastKTimes/3213-CountSubarraysWhereMaxElementAppearsAtLeastKTimes.py
@@ -0,0 +1,24 @@
+# Last updated: 10/22/2025, 12:08:45 AM
+class Solution:
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        maxi = max(nums)
+        i, j, cnt, ans = 0, 0, 0, 0
+
+        while j < n:
+            if nums[j] == maxi:
+                cnt += 1
+            
+            if cnt >= k:
+                while cnt >= k:
+                    ans += n - j
+                    if nums[i] == maxi:
+                        cnt -= 1
+                    i += 1
+                    
+            j += 1
+
+        return ans
+        
+
+        
