1f2b72d95690fb7a0160f538ac7068ce5326d298
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2025-10-22 00:09:47 -0400

    Create 203-RemoveLinkedListElements

---
+++ 203-RemoveLinkedListElements/203-RemoveLinkedListElements.py
@@ -0,0 +1,24 @@
+# Last updated: 10/22/2025, 12:09:47 AM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        if not head:
+            return head
+
+        node = head
+        while node and node.next:
+            if node.next.val == val:
+                node.next = node.next.next
+
+            else:
+                node = node.next
+
+        if head.val == val:
+            head = head.next
+
+        return head
+        
