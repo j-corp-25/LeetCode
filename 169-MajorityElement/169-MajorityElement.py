1d1dd4fcac5d6554c02dc018488e4d7a6b5b2527
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2025-10-22 00:09:50 -0400

    Create 169-MajorityElement

---
+++ 169-MajorityElement/169-MajorityElement.py
@@ -0,0 +1,17 @@
+# Last updated: 10/22/2025, 12:09:50 AM
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        my_map = {}
+
+        for num in nums:
+            if num not in my_map:
+                my_map[num] = 1
+            else:
+                my_map[num] += 1
+        
+        for num in my_map:
+            if my_map[num] > (len(nums) // 2):
+                return num
+            
+        return -1
+        
