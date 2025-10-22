7f73a044e7a4449b4cd1db448fe847b54b76c09e
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2025-10-22 00:09:42 -0400

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.py
@@ -0,0 +1,24 @@
+# Last updated: 10/22/2025, 12:09:42 AM
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # use two pointers, both  at beginning and then compare current with the next
+        # if theres a zero then swap current index with previous index
+
+
+
+
+
+        # Start search_zero at 1
+        search_zero = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                # Swap the current element with the previous non-zero element
+                nums[i], nums[search_zero] = nums[search_zero], nums[i]
+                search_zero += 1
+
+
+        
