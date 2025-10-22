db282653c6b5de62a461d879bef039035f276f28
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2025-10-22 00:02:44 -0400

    Time: 0 ms (100.00%) | Memory: 54.5 MB (38.25%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} word
+ * @param {character} ch
+ * @return {string}
+ */
+var reversePrefix = function(word, ch) {
+    let wordArray = word.split("")
+    let left = 0
+    let right = wordArray.indexOf(ch)
+
+    while(left < right){
+        [wordArray[left],wordArray[right]] = [wordArray[right],wordArray[left]]
+        left++
+        right--
+    }
+
+    return wordArray.join("")
+    
+};
