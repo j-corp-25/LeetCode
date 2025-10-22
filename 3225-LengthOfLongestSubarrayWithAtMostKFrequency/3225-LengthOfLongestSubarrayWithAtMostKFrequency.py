8677640a0e8e6c8388d9bb10861bafb27ee44276
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2025-10-22 00:08:43 -0400

    Create 3225-LengthOfLongestSubarrayWithAtMostKFrequency

---
+++ 3225-LengthOfLongestSubarrayWithAtMostKFrequency/3225-LengthOfLongestSubarrayWithAtMostKFrequency.py
@@ -0,0 +1,15 @@
+# Last updated: 10/22/2025, 12:08:43 AM
+class Solution:
+    def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        m = defaultdict(int)
+        i, j, ans = 0, 0, 1
+        while i < n and j < n:
+            m[nums[j]] += 1
+            while m[nums[j]] > k:
+                m[nums[i]] -= 1
+                i += 1
+            ans = max(ans, j - i + 1)
+            j += 1
+        return ans
+        
