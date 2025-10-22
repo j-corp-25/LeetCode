f5b8fcb9be91efc93967e5bebc213291a4ba1b3e
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2025-10-22 00:08:52 -0400

    Create 2442-NumberOfArithmeticTriplets

---
+++ 2442-NumberOfArithmeticTriplets/2442-NumberOfArithmeticTriplets.py
@@ -0,0 +1,13 @@
+# Last updated: 10/22/2025, 12:08:51 AM
+class Solution:
+    def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
+        s = set(nums)
+        # put all the numbers in a set {1,3,5,7,9}
+        count = 0
+        # return a count at the end
+        for i in range(len(nums)):
+            # iterate through each number in the nums array
+            if (nums[i] + diff) in s and (nums[i] + 2*diff) in s:
+                # here we check if (current num)1 + 2 = 3 is in the set which it is. then we check 1 + 2*2 = 5. which is also in the set we then increment count
+                count += 1
+        return count
