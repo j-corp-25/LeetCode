81232ee12e0909af506bfeae8d5019523f226d40
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2025-10-22 00:09:44 -0400

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.js
@@ -0,0 +1,43 @@
+// Last updated: 10/22/2025, 12:09:44 AM
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+// Big O( s + t) , O(n)
+// Memory (s + t), O(n)
+var isAnagram = function(s, t) {
+    if (s.length !== t.length){
+        return false
+    }
+    
+    let countS = {}
+    let countT = {}
+    
+    for(i = 0; i < s.length; i++){
+        if(!countS[s[i]]){
+            countS[s[i]] = 1
+        }else {
+            countS[s[i]] += 1 
+        }
+    }
+    // add increment to the characters of the j string, if the character is not a key make it 1, if it is already there then increment the count + 1
+    for(j = 0; j < t.length; j++){
+        if(!countT[t[j]]) {
+            countT[t[j]] = 1
+        }else {
+            countT[t[j]] += 1
+        }
+    }
+    // check the keys if they match between t and s
+    // Big O of (O(n)) time since it is just a for loop, which is linear
+    for (let key in countT) {
+        if (countS[key] !== countT[key]) {
+            return false;
+        }
+    }
+
+    return true;
+    
+};
