4cfc5b7e3b2108ed156ff1fddec22a8217b7aa30
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2025-10-22 00:09:24 -0400

    Create 713-SubarrayProductLessThanK

---
+++ 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.py
@@ -0,0 +1,24 @@
+# Last updated: 10/22/2025, 12:09:24 AM
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        if k <= 1:
+        
+            return 0
+
+        product = 1
+        result = 0
+
+        left = 0
+        right = 0
+
+        while right < len(nums):
+            product *= nums[right]
+            while product >= k:
+                product /= nums[left]
+                left += 1
+            result += right - left + 1
+            right += 1
+
+        return result
+
+        
