0e638ec76f78f60d9759e3c1a9970411e5bb5670
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2025-10-26 01:51:48 -0400

    Time: 19 ms (52.12%) | Memory: 56.2 MB (91.25%) - LeetSync

---
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var maxVowels = function (s, k) {
+    let vowels = 'aeiou'
+    let answer = 0
+    let vowelCounter = 0
+
+    for (let right = 0; right < k; right++) {
+        if (vowels.includes(s[right])) {
+            vowelCounter += 1
+        }
+    }
+    answer = vowelCounter
+    for (let i = k; i < s.length; i++) {
+       
+
+            if (vowels.includes(s[i - k])) {
+                vowelCounter -= 1
+            }
+            if (vowels.includes(s[i])) {
+                vowelCounter += 1
+            }
+            answer = Math.max(answer, vowelCounter)
+        
+
+
+    }
+    return answer
+}
