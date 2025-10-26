c03c674702b2b2a3fdeb43675737d585d13a4375
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2025-10-26 13:48:45 -0400

    Create 2351-FirstLettertoAppearTwice

---
+++ 2351-FirstLettertoAppearTwice/2351-FirstLettertoAppearTwice.js
@@ -0,0 +1,15 @@
+// Last updated: 10/26/2025, 1:48:45 PM
+/**
+ * @param {string} s
+ * @return {character}
+ */
+var repeatedCharacter = function(s) {
+    let myHashSet = new Set()
+    for(let i = 0;i<s.length;i++){
+        if(myHashSet.has(s[i])){
+            return s[i]
+        }
+        myHashSet.add(s[i])
+    }
+    
+};
